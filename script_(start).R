46d348d0d1ee4edcdff9d83da4eda41b40043d36
dragicalj <75049146+dragicalj@users.noreply.github.com>
2024-04-14 11:18:16 +0200

    Minor adjustments

--- script_(start).R
+++ script_(start).R
@@ -50,22 +50,6 @@
 # print the number of missing Cabin values in the train and test sets
 
 
-#install.packages('Amelia')
-# load Amelia library
-
-
-# set the display area to show two plots in the same row 
-
-
-# use the missmap f. to visualise the missing data in the train set
-
-
-# use the missmap f. to visualise the missing data in the test set
-
-
-# revert the plotting area to the default (one plot per row)
-
-
 ###########################
 # Handling missing values
 ###########################
@@ -179,88 +163,6 @@
 # merge train and test sets
 
 
-##################################
-## Creating an age proxy variable
-##################################
-
-# print a sample of the Name variable
-
-
-# split the name of the first observation on , or . characters
-
-
-# split the name of the first observation on , or . characters and unlist
-
-
-# split the name of the first observation on , or . characters, unlist and take the 2nd elem.
-
-
-# create a variable Title based on the value of the Name variable 
-
-
-# remove the leading space character from the Title
-
-
-# print the contingency table for the Title values
-
-
-# create a vector of all women (adult female) titles
-adult.women <- c("Dona", "Lady", "Mme", "Mrs", "the Countess")
-
-# create a vector of all girl (young female) titles
-girls <- c("Ms", "Mlle", "Miss")
-
-# create a vector of all men (adult male) titles
-adult.men <- c("Capt", "Col", "Don", "Dr", "Major", "Mr", "Rev", "Sir")
-
-# create a vector of all boy (young male) titles
-boys <- c("Master", "Jonkheer")
-
-# introduce a new character variable AgeGender
-
-
-# set the AgeGender value based on the vector the Title value belongs to
-
-
-# print the contingency table for the AgeGender values
-
-
-# plot the distribution of the Age attribute in the Young_Female group
-
-
-
-# plot the distribution of the Age attribute in the Adult_Male group
-
-
-
-# set the AgeGender to 'Adult_Female' for all 'girls' with age over 18
-
-
-# print the number of adult males who has the Age value set
-
-
-# set the AgeGender to 'Young_Male' for all 'Adult_Male' with age under 18
-
-
-
-# print the contingency table for the AgeGender variable
-
-
-# print the proportions table for the AgeGender variable
-
-
-# transform the AgeGender to factor
-
-
-
-# plot the AgeGender against Survived attribute
-
-
-
-# plot the AgeGender vs. Survived but as proportions
-
-
-
 ###################################
 ## Creating the FamilySize variable
 ###################################
@@ -285,67 +187,3 @@ boys <- c("Master", "Jonkheer")
 
 
 # plot the FamilySize vs. Survived
-
-
-#####################################
-## Making use of the Ticket variable
-#####################################
-
-# print a sample of Ticket values
-
-
-# compute the number of distinct values of the Ticket variable
-
-
-# use tapply to compute the number of passengers on the same ticket
-
-
-
-# create a data frame with ticket name and ticket count as variables
-
-
-# print first few rows of the new data frame
-
-
-# print the contingency table of the count variable
-
-
-# merge titanic.all and ticket.count.df datasets on the Ticket variable
-
-
-
-# change the name of the newly added column to PersonPerTicket
-
-
-# print the contingency table of the PersonPerTicket variable
-
-
-# set the PersonPerTicket to 4 to all observations where PersonPerTicket > 4
-
-
-# convert PersonPerTicket to factor
-
-
-
-# print the contingency table for the PersonPerTicket
-
-
-# print the contingency table for the PersonPerTicket vs. FamilySize
-
-
-# plot all survived passangers (without NAs)
-
-
-# plot the PersonPerTicket vs. Survived using proportions
-
-
-
-##################################
-# Save the augmented data set
-##################################
-
-# split into train and test set based on whether the Survived is present
-
-
-# save both data sets to a file
-
